bd82e419348c7c08d0cedca1879ae2c18118ea9f
Jarky0 <mail@jarky.de>
2025-04-03 18:49:05 +0200

    Change output filename to power-flow-card-plus-conditional.js for correct HACS path

--- rollup.config.js
+++ rollup.config.js
@@ -24,12 +24,12 @@ export default [
     input: ["src/power-flow-card-plus.ts"],
     output: [
       {
-        file: "dist/power-flow-card-plus.js",
+        file: "dist/power-flow-card-plus-conditional.js",
         format: "es",
         inlineDynamicImports: true,
       },
       {
-        file: "release/power-flow-card-plus.js",
+        file: "release/power-flow-card-plus-conditional.js",
         format: "es",
         inlineDynamicImports: true,
       },
